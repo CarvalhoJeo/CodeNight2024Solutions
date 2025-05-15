bfda34f60e28c1d8cb138ab38456c935d0e29a41
Carvalho <lucacarvalhojeo@gmail.com>
2025-05-15 00:47:26 -0300

    update template

--- template.cpp
+++ template.cpp
@@ -1,34 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define fast_io                       \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL);
-#define ll long long
-#define vi vector<int>
-#define vll vector<long long>
-#define pii pair<int, int>
-#define pb push_back
-#define all(v) v.begin(), v.end()
-#define sz(x) (int)(x.size())
-#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
-
-const int MOD = 1e9 + 7;
-const int INF = 1e9;
-
-void solve()
-{
-    // Write your solution here
-}
-
 int main()
 {
-    fast_io;
-    int t = 1;
+    int t;
     cin >> t;
-    while (t--)
-    {
-        solve();
-    }
     return 0;
 }
